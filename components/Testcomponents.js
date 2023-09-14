106013ee6b455c383007956d341ecbdef5186b91
krys11 <krysdario1996@gmail.com>
2023-09-14 01:11:27 +0100

    all

---
+++ components/Testcomponents.js
@@ -0,0 +1,28 @@
+// In your component -- TestComponent
+import { useKkiapay } from "@kkiapay-org/react-native-sdk";
+import { useEffect } from "react";
+import { Button, View } from "react-native";
+
+export default function TestComponent() {
+  const { openKkiapayWidget, addSuccessListener } = useKkiapay();
+
+  useEffect(() => {
+    addSuccessListener((data) => {
+      console.log("transactionId: ", data);
+    });
+  }, []);
+
+  const openWidget = () => {
+    openKkiapayWidget({
+      amount: 100,
+      key: "7acd0660528c11eebc8313ef873f17d5",
+      sandbox: true,
+    });
+  };
+
+  return (
+    <View>
+      <Button title="Pay now" onPress={openWidget} />
+    </View>
+  );
+}
--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,12 +1,17 @@
+import { KkiapayProvider } from "@kkiapay-org/react-native-sdk";
 import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import TestComponent from "../../components/Testcomponents";
 
 const Home = ({ route }) => {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Text>Home</Text>
+        <KkiapayProvider>
+          {/* <Text>Home</Text> */}
+          <TestComponent />
+        </KkiapayProvider>
       </SafeAreaView>
     </SafeAreaProvider>
   );
