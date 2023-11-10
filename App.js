f6f5cabbb137ea25f7fbd1437c4051475a152f8e
krys11 <krysdario1996@gmail.com>
2023-11-10 12:27:40 -0800

    all

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Entry from "./screens/Entry";
 import * as Notifications from "expo-notifications";
 import { ToastConfig } from "./components/Toastcomponent";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -15,9 +16,9 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <Entry />
       <ToastConfig />
-    </>
+    </SafeAreaProvider>
   );
 }
